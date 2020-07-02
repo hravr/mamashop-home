3a6f09d109ec9027a4d096fb15e18f78dac743e8
vadvit009 <vadvit009@gmail.com>
2020-07-02 19:31:09 +0300

    node+react deploy

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+// init express
+const app = express();
+
+// access cors (temporary)
+app.use(cors());
+
+// use static
+app.use(express.static(path.resolve(__dirname, './build')));
+
+app.use('*', (req, res) => {
+  res.send(
+    express.static(path.resolve(__dirname, './build/index.html'))
+  )
+})
+
+// handle 404 (user errors)
+app.use((req, res, next) => {
+  res.status(404).send(`<h2>Oops 404</h2> <h3>We think you are lost!</h3>`);
+});
+
+// start listening
+app.listen(9000, () => console.log(`app started on 9000`));
