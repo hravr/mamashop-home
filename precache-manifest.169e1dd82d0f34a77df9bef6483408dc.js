a60018b2a79d7874fe88f3d42bdd4916f3eed63b
Jokerweb <jokerwebdev@gmail.com>
2020-07-02 17:51:52 +0300

    start mama project

---
+++ precache-manifest.169e1dd82d0f34a77df9bef6483408dc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f9a705eb2825cfc24481f13d89e55c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f96dcffd2703e9b1c09c",
+    "url": "/static/css/main.428faf3e.chunk.css"
+  },
+  {
+    "revision": "0bd1fd50d6dd34aa4199",
+    "url": "/static/js/2.5dd8e561.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5dd8e561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f96dcffd2703e9b1c09c",
+    "url": "/static/js/main.e6f239f6.chunk.js"
+  },
+  {
+    "revision": "0a917bc8c78231d3e245",
+    "url": "/static/js/runtime-main.339146cf.js"
+  },
+  {
+    "revision": "d6c432e75a33cbdb859236f19a448ac2",
+    "url": "/static/media/baby.d6c432e7.svg"
+  },
+  {
+    "revision": "d65d0ad183a5ca4e221317eaee89b5f6",
+    "url": "/static/media/boy.d65d0ad1.svg"
+  },
+  {
+    "revision": "12941d96aec4a9fa9d2a45bfa0b43ba9",
+    "url": "/static/media/car.12941d96.svg"
+  },
+  {
+    "revision": "2e84af035f795b650956ca490d23194a",
+    "url": "/static/media/chat.2e84af03.svg"
+  },
+  {
+    "revision": "6f35e9b2276c02bf1149e77af27cd454",
+    "url": "/static/media/clothes.6f35e9b2.svg"
+  },
+  {
+    "revision": "6dc6f52da34b433156f2cedc74efa65b",
+    "url": "/static/media/girl.6dc6f52d.svg"
+  },
+  {
+    "revision": "1624f1390b3ccdd98290856519601ffe",
+    "url": "/static/media/img.1624f139.svg"
+  },
+  {
+    "revision": "a4d17a7996cd810df48189072694e06d",
+    "url": "/static/media/logo.a4d17a79.svg"
+  },
+  {
+    "revision": "0c78603032a2aa6fbacc6222904790e9",
+    "url": "/static/media/money.0c786030.svg"
+  },
+  {
+    "revision": "7ca25a82f8bc03f8f56671c2fbc8fa9a",
+    "url": "/static/media/robot.7ca25a82.svg"
+  },
+  {
+    "revision": "34220cdf9a87b09d2017988f8fb62b8f",
+    "url": "/static/media/toys.34220cdf.svg"
+  }
+]);
